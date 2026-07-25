71d21cb1f0d85216b316fab4f421a30ca5bac7dd
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-25 19:40:10 +0530

    Create Proram2.c

---
+++ Proram2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++)
+        sum += a[i];
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
