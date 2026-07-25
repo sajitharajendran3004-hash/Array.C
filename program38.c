11b172dff042930c29d9af6b8bccfbb2b7197dbd
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-25 20:40:27 +0530

    Implement first non-repeated element finder

---
+++ program38.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, j, count;
+
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    for(i=0;i<n;i++){
+        count=0;
+
+        for(j=0;j<n;j++){
+            if(a[i]==a[j])
+                count++;
+        }
+
+        if(count==1){
+            printf("First Non-Repeated = %d",a[i]);
+            return 0;
+        }
+    }
+
+    printf("No Unique Element");
+
+    return 0;
+}
