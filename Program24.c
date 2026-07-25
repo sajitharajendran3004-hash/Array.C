a1fdeac2aa55ef825220f562ac87076e327e6fd7
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-25 20:32:13 +0530

    Create Program24.c

---
+++ Program24.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, j, k;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n;) {
+            if(a[i] == a[j]) {
+                for(k = j; k < n - 1; k++)
+                    a[k] = a[k + 1];
+                n--;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    printf("Array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
